599addec55cb3a0c987a4b2346d774ce977b5ef9
Persi Priscilla <136316927+Persi23@users.noreply.github.com>
2025-09-18 13:36:10 +0530

    Ex1.php

---
+++ Ex1.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Welcome to Our Website</h1>
+<form action="welcome.php" method="post"> Name: <input type="text" name="name"><br><br> <input type="submit">
+</form>
+</body>
+</html>
